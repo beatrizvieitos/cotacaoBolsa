3745d3274ef29d372b9579727baeb5af8a8b5999
Beatriz Vieitos <bea.macarico@gmail.com>
2026-01-21 11:33:48 +0000

    Colocação de inputs para filtrar resultados

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import yfinance as yf
 
+acao = input("Insira o nome da ação em bolsa que deseja consultar: ")
+inicio = input("Insira a data de início da cotação: ")
+fim = input("Insira a data de fim da cotação: ")
+
 #Obter os dados de cotação em bolsa
-df= yf.download('TSLA', start='2025-12-01', end='2025-12-31')
+df= yf.download(acao, start=inicio, end=fim)
 
 print(df)
