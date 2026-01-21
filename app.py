23d3563aca9f1e6185749b988cf5fd37127532d6
Beatriz Vieitos <bea.macarico@gmail.com>
2026-01-21 11:50:00 +0000

    Adicionar Loop para ter opção para voltar a perguntar se quero consultar mais ações

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import yfinance as yf
 
-acao = input("Insira o nome da ação em bolsa que deseja consultar: ")
-inicio = input("Insira a data de início da cotação: ")
-fim = input("Insira a data de fim da cotação: ")
+opcao = "" #Variável de controle
 
-#Obter os dados de cotação em bolsa
-df= yf.download(acao, start=inicio, end=fim)
+while True:
+    acao = input("Insira o nome da ação em bolsa que deseja consultar: ")
+    inicio = input("Insira a data de início da cotação: ")
+    fim = input("Insira a data de fim da cotação: ")
 
-print(df)
+    #Obter os dados de cotação em bolsa
+    df= yf.download(acao, start=inicio, end=fim)
+
+    print(df)
+
+    opcao = input("Deseja consultar outra ação? (Sim/Não): ").lower()
+
+    if opcao == "não":
+        print("Operação finalizada.")
+        break
+
